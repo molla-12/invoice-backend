a62b6bd230e8b3122cb3fdb9e415eef8aefbe6ac
MOLLA <mollaman19@gmail.com>
2024-06-24 11:28:13 +0300

    vercel

--- api/index.php
+++ api/index.php
@@ -1,14 +1,3 @@
-<?php
+<?php  
 
-// This file is required by Vercel for handling serverless functions
-
-// Bootstrap Laravel
-define('LARAVEL_START', microtime(true));
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . "/../public/index.php";
