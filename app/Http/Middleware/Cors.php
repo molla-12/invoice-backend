ad3a8398a27fa44d7d7385fa5908fe06251800f4
MOLLA <mollaman19@gmail.com>
2024-06-24 13:43:15 +0300

    fix3

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'https://invoice-frontend-nine.vercel.app');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
+
+        return $response;
+    }
+}
