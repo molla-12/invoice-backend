f39f6bd5dc16f9de5f92221596c0a8283241df68
MOLLA <mollaman19@gmail.com>
2024-06-24 14:20:56 +0300

    update

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        // 'api/*', // Exclude all routes under 'api' prefix from CSRF verification
+        'api/*', // Exclude all routes under 'api' prefix from CSRF verification
     ];
 }
